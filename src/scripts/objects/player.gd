3ebb2d51ff8809d1236930d8a9cfe49fc0a4ab88
SmallConfusion <smallconfusion@protonmail.com>
2024-07-05 06:47:45 -0500

    Added basic player

---
+++ src/scripts/objects/player.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+const SPEED := 1600.0
+
+
+
+func _physics_process(delta):
+	var pointing_direction := (get_global_mouse_position() - global_position)
+	var force := Vector2.ZERO
+
+	rotation += (pointing_direction.angle() - rotation) * delta * 3.
+	
+	if Input.is_action_pressed("Fire"):
+		force = -Vector2.from_angle(rotation) * SPEED
+	
+	apply_central_force(force)
