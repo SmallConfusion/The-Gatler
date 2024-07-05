7c52df3a0d1c29dab568b36d927e5dc85d36ec7a
SmallConfusion <smallconfusion@protonmail.com>
2024-07-05 08:25:53 -0500

    fixed rotation bug

--- src/scripts/objects/player.gd
+++ src/scripts/objects/player.gd
@@ -1,12 +1,20 @@
 extends RigidBody2D
 
 const SPEED := 1600.0
+const ROTATION_CHANGE_FORCE := 3.
 
 func _physics_process(delta):
 	var pointing_direction := (get_global_mouse_position() - global_position)
 	var force := Vector2.ZERO
 
-	rotation += (pointing_direction.angle() - rotation) * delta * 3.
+	var rotation_change : float = pointing_direction.angle() - rotation
+	
+	if rotation_change > PI:
+		rotation_change -= TAU
+	elif rotation_change < -PI:
+		rotation_change += TAU
+	
+	rotation += rotation_change * ROTATION_CHANGE_FORCE * delta
 	
 	if Input.is_action_pressed("Fire"):
 		force = -Vector2.from_angle(rotation) * SPEED
