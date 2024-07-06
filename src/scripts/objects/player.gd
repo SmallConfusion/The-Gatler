976e8ace99f0f183796b81b2094c97f2ff533b52
SmallConfusion <smallconfusion@protonmail.com>
2024-07-06 06:27:15 -0500

    Added gun heat up shader

--- src/scripts/objects/player.gd
+++ src/scripts/objects/player.gd
@@ -5,6 +5,11 @@ const ROTATION_CHANGE_FORCE := 8.
 
 @onready var gun_audio_tween : Tween
 
+var gun_heat := 0.0
+
+var heat_up_speed := 0.4
+var cool_down_speed := 1.0
+
 
 func _physics_process(delta):
 	var pointing_direction := (get_global_mouse_position() - global_position)
@@ -22,9 +27,15 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Fire"):
 		force = -Vector2.from_angle(rotation) * SPEED
 		$Gun.fire_cooldown()
+		
+		gun_heat += delta * heat_up_speed
+	else:
+		gun_heat -= delta * cool_down_speed
 	
 	apply_central_force(force)
-
+	
+	RenderingServer.global_shader_parameter_set("gun_heat", gun_heat)
+	
 	$Skeleton2D/HeadTarget.global_position = lerp($Skeleton2D/HeadTarget.global_position, global_position + Vector2(100, 0).rotated((-pointing_direction).angle()), delta * 20)
 
 
