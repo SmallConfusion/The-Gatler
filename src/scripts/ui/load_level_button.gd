318f2b0b4f810e6eb7df802abb929cb601f4d6c5
SmallConfusion <smallconfusion@protonmail.com>
2024-07-07 12:07:05 -0500

    Fixed cutscene text

--- src/scripts/ui/load_level_button.gd
+++ src/scripts/ui/load_level_button.gd
@@ -4,9 +4,10 @@ class_name LoadLevelButton
 @export var level_index := 0
 
 func _ready():
-	var debug_settings : DebugSettings = get_tree().get_first_node_in_group("debug_settings")
-	
-	if Global.unlocked_level < level_index and (not debug_settings or not debug_settings.unlock_all_levels):
+	if Global.unlocked_level < level_index:
 		disabled = true
 	
+	var debug_settings : DebugSettings = get_tree().get_first_node_in_group("debug_settings")
 	
+	if OS.is_debug_build() and debug_settings and debug_settings.unlock_all_levels:
+		disabled = false
