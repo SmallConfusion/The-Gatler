b09d8c4954f9dd5dee72da9dec94e958fcfd7bd9
SmallConfusion <smallconfusion@protonmail.com>
2024-07-07 14:07:37 -0500

    fixed bug

--- src/scripts/components/reload_on_die.gd
+++ src/scripts/components/reload_on_die.gd
@@ -4,4 +4,4 @@ class_name ReloadOnDeath
 func _ready():
 	for sibling in get_parent().get_children():
 		if sibling is Death:
-			sibling.connect("killed", get_tree().reload_current_scene)
+			sibling.connect("killed", get_tree().change_scene_to_file.bind("res://scenes/menu/main_menu.tscn"))
